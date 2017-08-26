8fe62d1422d62217ecde679884deb86b2562a0da
Mel Florance <mel.florance@gmail.com>
2017-08-26 19:19:48 +0200

    Update new example game with new engine updates !

--- src/game/game.js
+++ src/game/game.js
@@ -1,38 +1,37 @@
 var engine = new FNX.Engine('render');
 
-var matrix  = new FNX.Mat4();
-var mesh    = new FNX.Mesh();
-var assets  = new FNX.Assets();
-mesh.shader = new FNX.Shader();
+var assets = new FNX.Assets();
 
-mesh.addVertices([
-    new FNX.Vertex(new FNX.Vec3(-1, -1, 0)),
-    new FNX.Vertex(new FNX.Vec3(1, -1, 0)),
-    new FNX.Vertex(new FNX.Vec3(0, 1, 0))
-]);
 
-assets.loadShader('basic').then(resource =>
+assets.loadMesh('box').then(mesh =>
 {
-    mesh.shader.addVertexShader(resource.vertex);
-    mesh.shader.addFragmentShader(resource.fragment);
-    mesh.shader.compile();
-    mesh.shader.addUniform('transform');
+    mesh.shader = new FNX.Shader();
+    assets.loadShader('basic').then(resource =>
+    {
+        mesh.shader.addVertexShader(resource.vertex);
+        mesh.shader.addFragmentShader(resource.fragment);
+        mesh.shader.compile();
+        mesh.shader.addUniform('transform');
 
-    let transform = new FNX.Transform();
-    let t = 0;
+        let transform = new FNX.Transform();
+        transform.setProjection(70, engine.canvas.width, engine.canvas.height, 0.1, 100);
+        let t = 0;
 
-    engine.run(() => {
-        t += engine.deltaTime;
+        engine.run(() => {
+            t += engine.deltaTime;
 
-        if(mesh.shader.compiled) {
-            // transform.setTranslation(Math.sin(t), Math.cos(t), 0);
-            transform.setRotation(Math.sin(t), Math.sin(t), Math.sin(t));
-            mesh.shader.bind();
-            mesh.shader.setUniformMat4('transform', transform.getTransformation());
-            mesh.draw();
-        }
+            if(mesh.shader.compiled) {
+                transform.translate([Math.sin(t), 0, -5]);
+                transform.scale([Math.sin(t),Math.sin(t),Math.sin(t)]);
+                transform.rotate(Math.sin(t) * 2 * Math.PI, [0,1,1]);
+                mesh.shader.bind();
+                mesh.shader.setUniformMat4('transform', transform.getProjectedTransform());
+                mesh.draw();
+            }
 
-        if(engine.input.getKey(13))
-            console.log('pressed')
+            if(engine.input.getKey(13))
+                console.log('pressed')
+        });
     });
 });
+
